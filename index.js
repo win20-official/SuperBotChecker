d7eb0af2dc884b814a5148e0eba966e034d7aab3
Windows <kasiak0101@gmail.com>
2022-11-29 22:05:21 +0100

    Add new files

---
+++ index.js
@@ -0,0 +1,59 @@
+const dotenv = require("dotenv")
+dotenv.config()
+const { Client, GatewayIntentBits, REST, Routes, ApplicationCommandOptionType } = require("discord.js")
+const client = new Client({ intents: [GatewayIntentBits.Guilds] })
+
+const LOGIN = process.env.TOKEN
+const commands = [
+  {
+    name: "botcheck",
+    description: "Check if a user is a bot",
+    options: [
+        {
+          name: "user",
+          description: "The required user.",
+          required: true,
+          type: ApplicationCommandOptionType.User
+        }
+    ]
+  }
+]
+
+const rest = new REST({ version: "10" }).setToken(LOGIN)
+
+;(async () => {
+  try {
+    console.log("Started refreshing application (/) commands.")
+
+    await rest.put(Routes.applicationCommands("1046460605315424366"), {
+      body: commands,
+    })
+
+    console.log("Successfully reloaded application (/) commands.")
+  } catch (error) {
+    console.error(error)
+  }
+})()
+
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`)
+})
+
+client.on("interactionCreate", async (interaction) => {
+  if (!interaction.isChatInputCommand()) return
+
+  if (interaction.commandName === "botcheck") {
+    const username = interaction.options.getUser("user")
+    const text = "is not a bot."
+
+    if (username.bot === false) {
+      await interaction.reply(`The user ${username} ${text}`)
+    } else {
+      const text = "is a bot."
+      await interaction.reply(`The user ${username} ${text}`)
+    }
+
+  }
+})
+
+client.login(LOGIN)
